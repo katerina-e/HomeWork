c1dd126fd98f713f733d53f64c07d49a833fd996
katerina-e <st077304@student.spbu.ru>
2020-10-08 04:18:15 +0300

    third commit

--- hw2.cpp
+++ hw2.cpp
@@ -6,49 +6,129 @@ using namespace std;
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
-void BrLine(int x1,int y1, int x2,int  y2,TGAImage image, TGAColor color){
+void BrLine(int x1,int y1, int x2,int  y2,TGAImage& image, TGAColor color){
 
-  int absdx = abs(x1-x2);
-  int absdy = abs(y1-y2);
-  int mist = 0;
-  int dmist= absdy+1;
-  int y = y1;
+  if(((x1>x2)&&(y1>y2))||((x1>x2)&&(y1<y2))){
+    int tx= x1;
+    int ty= y1;
+    x1 = x2;
+    x2 = tx;
+    y1 = y2;
+    y2 = ty;
+  };
+
+  int dx = x2-x1;
   int dy = y2-y1;
-  if (dy>0){
-     dy = 1;
-  };
-  if (dy<0){
-     dy = -1;
-  };
-  for  (int i= x1; i<= x2; i++){
-     image.set(i,y, color);
-     mist = mist+ dmist;
-     if (mist >= (absdx+1)){
-        y = y + dy;
-        mist = mist - (absdx+1);
-     };
-  };
+
+  if(dy>0){
+    if(dx>=dy){
+     int x =x1;
+     int y =y1;
+     int d = 2*dy -dx;
+     while(x<=x2){
+       image.set(x,y,color);
+       if(d<0)
+          d= d+2*dy;
+       else{
+          d= d+ 2*(dy-dx);
+          y= y+1;
+       }
+       x= x+1;
+     }
+    }
+    else{
+      int x =x1;
+      int y =y1;
+      int d = 2*dx -dy;
+      while(y<=y2){
+        image.set(x,y,color);
+        if(d<0)
+          d= d+2*dx;
+        else{
+          d= d+ 2*(dx-dy);
+          x= x+1;
+        }
+      y= y+1;
+      }
+    }
+  }
+  else{
+    int absdy = abs(dy);
+    if(dx>=absdy){
+     int x =x1;
+     int y =y1;
+     int d = 2*absdy -dx;
+     while(x<=x2){
+       image.set(x,y,color);
+       if(d<0)
+          d= d+2*absdy;
+       else{
+          d= d+ 2*(absdy-dx);
+          y= y-1;
+       }
+       x= x+1;
+     }
+    }
+    else{
+     int x =x1;
+     int y =y1;
+     int d = 2*dx-absdy;
+     while(y>=y2){
+       image.set(x,y,color);
+       if(d<0)
+          d= d+2*dx;
+       else{
+          d= d+ 2*(dx-absdy);
+          x= x+1;
+       }
+       y= y-1;
+     }
+    }
+
+  }
 
 return;
 }
-void CDAline (int x1, int y1, int x2, int y2,TGAImage image, TGAColor color){
+void CDAline (int x1, int y1, int x2, int y2,TGAImage& image, TGAColor color){
 
   int dx = x2-x1;
   int dy = y2-y1;
   int absdx = abs(dx);
   int absdy = abs(dy);
-  int m= max(absdx,absdy);
-  int e= 1/m;
-  int y = y1;
-  for (int x= x1; x<=(x1+absdx); x++){
-     image.set(x,y,color);
-     x= x+ e*dx;
-     y= y+ e*dy;
-  }
+  int x =x1;
+  int y =y1;
+
+  if(((x1>x2)&&(y1>y2))||((x1>x2)&&(y1<y2))){
+    int tx= x1;
+    int ty= y1;
+    x1 = x2;
+    x2 = tx;
+    y1 = y2;
+    y2 = ty;
+  };
+
+     if(dx<dy){
+       int d1x = 1;
+       int d1y = absdy/absdx;
+       while(x<=x2){
+          image.set(x,y,color);
+          x = x+d1x;
+          y = y+d1y;
+      }
+     }
+    /* else{ cout<<"yes"<<endl;
+       int d1x = 1;
+       int d1y = absdy/absdx;
+       while(x<=x2){
+          image.set(x,y,color);
+          x = x+d1x;
+          y = y+d1y;
+      }
+     }*/
 return;
 }
 
-void CirclePar(int x0, int y0, int r,TGAImage image, TGAColor color){
+void CirclePar(int x0, int y0, int r,TGAImage& image, TGAColor color){
 
 int x2 = x0+r;
 int y2 = y0;
@@ -62,31 +142,35 @@ for(int i= 1; i<=360; i++){
 return;
 }
 
-void CirclePixel(int x0, int y0, int x, int y, TGAImage image, TGAColor color ){
+void CirclePixel(int x0, int y0, int x, int y, TGAImage& image, TGAColor color ){
 
-image.set(x0+x,y0+y,color);
-image.set(x0+y,y0+x,color);
-image.set(x0+y,y0-x,color);
-image.set(x0+x,y0-y,color);
+   image.set(x0+x,y0+y,color);
+   image.set(x0+y,y0+x,color);
+   image.set(x0+y,y0-x,color);
+   image.set(x0+x,y0-y,color);
 
-image.set(x0-x,y0-y,color);
-image.set(x0-y,y0-x,color);
-image.set(x0-y,y0+x,color);
-image.set(x0-x,y0+y,color);
+   image.set(x0-x,y0-y,color);
+   image.set(x0-y,y0-x,color);
+   image.set(x0-y,y0+x,color);
+   image.set(x0-x,y0+y,color);
 
 return;
 };
 
-void Circle (int x0, int y0, int r, TGAImage image, TGAColor color){
-
-for(int x=0; x<= (r/sqrt(2));x++){
-    int y = (int)(sqrt(r^2-x^2));
-    CirclePixel(x0, y0,x,y,image,color);
+void Circle (int x0, int y0, int r, TGAImage& image, TGAColor color){
+int R= (r*r)/2;
+int x=0;
+int y;
+while((x*x)<= R){
+    int tempr = (r-x)*(r+x);
+    y = sqrt(tempr);
+    CirclePixel(x0,y0,x,y,image,color);
+    x= x+1;
 }
 return;
 };
 
-void CircleBr (int x0, int y0,int r, TGAImage image, TGAColor color ){
+void CircleBr (int x0, int y0,int r, TGAImage& image, TGAColor color ){
 
 int x =0;
 int y =r;
@@ -105,10 +189,10 @@ while( y>= 0){
     }
     if ((d>0)&&(mist>0)){
         y= y-1;
-        d= d-(2*+1);
+        d= d-(2*y+1);
     }
     x= x+1;
-    y= y+1;
+    y= y-1;
     d= d+2*(x-y);
 }
 return;
@@ -116,11 +200,13 @@ return;
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(10, 10, red);
+	//BrLine(20,80,40,30,image, red);
+	image.set(50, 50, red);
+	CDAline(20,20,40,60,image,red);
+	//CircleBr (50,50,30,image,white);
+	//CirclePar(50,50,30,image,white);
+	//Circle(50,50,30,image,white);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-	//BrLine(25,25,50,70,image, red);
-	//CDAline(25,25,50,70,image,red);
-	Circle (45,56,7,image,white);
 	image.write_tga_file("output.tga");
 	return 0;
 }
